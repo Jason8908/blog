2900b520f5cbc415008794d6e9d02721a7318298
Jason8908 <jasonsu894@gmail.com>
2026-02-26 17:45:57 -0500

    updated home page again

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Heading from "@/components/ui/Heading"
 import Prose from "@/components/ui/Prose"
 import ProfilePhoto from "@/components/ui/ProfilePhoto"
 import { Link } from "react-router-dom"
+import { ArrowRightIcon } from "lucide-react"
 
 export default function Home() {
   return (
@@ -16,7 +17,7 @@ export default function Home() {
         <div>
           <Heading level={1}>Jason Su</Heading>
           <p className="mt-2 text-lg text-muted-foreground">
-            What makes life worth living?
+            How is it that you want to be remembered?
           </p>
         </div>
       </Section>
@@ -24,20 +25,23 @@ export default function Home() {
       <Section>
         <Prose className="mt-4">
           <p>
-            Much of my time recently has been spent on advancing my career; working on ways
+            Much of my time recently has been spent on advancing my career; I've been working on ways
             to improve my skills, and to get better at my craft. Almost all of the projects
             I've built in the last year or so have been to push this goal forward.
           </p>
           <p>
-            This site is different.
+            I wanted to try something different.
           </p>
           <p>
-            Most of the things I put on here won't be work related, and some of it might be a jumble
+            The things I put on here won't be work related, and some of it might be a jumble
             of incoherent thoughts; nonetheless, they serve as an insight into the inner machinations 
             of my mind.
           </p>
           <p>Not written with AI, obviously.</p>
-          <Link to="/chapters" className="text-primary underline">View Chapters</Link>
+          <Link to="/chapters" className="text-primary underline">
+            Chapters
+            <ArrowRightIcon className="ml-2 inline-block w-4 h-4" />
+          </Link>
         </Prose>
       </Section>
     </>
